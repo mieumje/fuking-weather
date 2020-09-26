1dde4ac3390484a9a959051b7c588c2aeaf3b7ef
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-26 18:56:17 +0900

    Loading Screen
    background color, text font size 수정

--- loading.js
+++ loading.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Loading() {
   return (
     <View style={style.container}>
-      <Text>Getting the fucking weather</Text>
+      <Text style={style.text}>Getting the fucking weather</Text>
     </View>
   );
 }
@@ -15,5 +15,10 @@ const style = StyleSheet.create({
     justifyContent: "flex-end",
     paddingHorizontal: 30,
     paddingVertical: 100,
+    backgroundColor: "#FEF5A2",
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 25,
   },
 });
