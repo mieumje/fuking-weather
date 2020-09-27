41866ac42e96d28d8bdc4728dc05c78de6549e05
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-27 17:37:26 +0900

    import expo location

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Loading from "./loading";
+import * as Location from "expo-location";
 
 export default function App() {
   return <Loading></Loading>;
