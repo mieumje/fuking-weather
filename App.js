d6f350b2f55295fb5dc2dbc81b44651603a15c61
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-29 21:40:56 +0900

    #1.3 Asking for Permissions
    위치를 알려주기 위해 permission을 추가,
    console 창에 latitude, longitude 출력

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
+import { Alert } from "react-native";
 import React from "react";
 import Loading from "./loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords } = await Location.getCurrentPositionAsync();
+      console.log(coords.latitude, coords.longitude);
+    } catch (error) {
+      Alert.alert("Can't find you.");
+    }
   };
   componentDidMount() {
     this.getLocation();
