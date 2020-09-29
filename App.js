88bba1107d2dffb6bd024f6de5208c9083555de2
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-29 22:15:25 +0900

    #2.0 Displaying Temperature
    temp를 화씨에서 섭씨로 변경

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class extends React.Component {
   };
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
     console.log(data);
   };
