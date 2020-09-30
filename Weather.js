ffb7c7872ea45244fc00d0ea16e7b366af760fb8
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-30 19:26:40 +0900

    weather
    날씨와 맞는 icon, gradient 삽입

--- Weather.js
+++ Weather.js
@@ -4,18 +4,60 @@ import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+  Haze: {
+    iconName: "weather-fog",
+    gradient: ["#4DA0B0", "#D39D38"],
+  },
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#948E99", "#2E1437"],
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#BBD2C5", "#536976"],
+  },
+  Rain: {
+    iconName: "weather-pouring",
+    gradient: ["#536976", "#292E49"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#E6DADA", "#274046"],
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#2980B9", "#6DD5FA"],
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#1F1C2C", "#928DAB"],
+  },
+  Mist: {
+    iconName: "weather-fog",
+    gradient: ["#BBD2C5", "#536976", "#292E49"],
+  },
+  Dust: {
+    iconName: "weather-fog",
+    gradient: ["#F0C27B", "#4B1248"],
+  },
+};
+
+export default function Weather({ temp, condition }) {
   return (
     <LinearGradient
       style={styles.container}
-      // Background Linear Gradient
-      colors={["rgba(0,0,0,0.8)", "transparent"]}
+      colors={weatherOptions[condition].gradient}
     >
       <StatusBar barStyle="light-content"></StatusBar>
       <View style={styles.halfContainer}>
         <MaterialCommunityIcons
           size={86}
-          name="weather-lightning-rainy"
+          name={weatherOptions[condition].iconName}
           color="white"
         />
         <Text style={styles.temp}>{temp}℃</Text>
